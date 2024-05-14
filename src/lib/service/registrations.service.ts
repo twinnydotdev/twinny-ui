ec1c6c3de79aefde483e527d9dae4fabb6614982
rjmacarthy <richardmacarthy@hotmail.com>
2024-05-14 08:39:07 +0100

    add service

---
+++ src/lib/service/registrations.service.ts
@@ -0,0 +1,25 @@
+import { fail } from '@sveltejs/kit'
+import { db } from '../database'
+import { verifyEmailAddress as getEmailValid } from '$lib/utils'
+
+export async function registerEmailAddress(request: Request) {
+  const formData = await request.formData()
+  const emailAddress = formData.get('email') as string
+  const emalValid = getEmailValid(emailAddress)
+  if (!emailAddress || !emalValid) return fail(400)
+  return await new Promise((resolve, reject) => {
+    try {
+      db.get('SELECT * FROM subscribers WHERE email = ?', [emailAddress], (err, subscriber) => {
+        if (subscriber) return reject()
+        db.prepare('INSERT INTO subscribers (email) VALUES (?)', [emailAddress]).run(
+          emailAddress,
+          () => {
+            return resolve({ success: true })
+          }
+        )
+      })
+    } catch (err) {
+      return reject()
+    }
+  })
+}
