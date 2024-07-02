c374c2c8629b23921e9f44635704a8fb02ea3133
rjmacarthy <richardmacarthy@hotmail.com>
2024-07-02 11:44:18 +0100

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name: 'twinny',
+    script: './build/index.js',
+    watch: false,
+    autorestart: true,
+    env: {
+        PORT: 3003,
+        ENV_PATH: "./.env",
+    }
+  }]
+}
