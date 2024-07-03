4f22f38202b278a6b10addb29930614b38578f21
rjmacarthy <richardmacarthy@hotmail.com>
2024-07-03 12:57:10 +0100

    update links

--- src/lib/const/index.ts
+++ src/lib/const/index.ts
@@ -1,6 +1,6 @@
 export const URL_VSCODE_MARKETPLACE =
   'https://marketplace.visualstudio.com/items?itemName=rjmacarthy.Twinny'
-export const URL_GITHUB = 'https://github.com/rjmacarthy/twinny'
-export const URL_DOCS = 'https://rjmacarthy.github.io/twinny-docs/'
-export const URL_RJMACARTHY = 'https://x.com/rjmacarthy'
+export const URL_GITHUB = 'https://github.com/twinnydotdev/twinny'
+export const URL_DOCS = 'https://twinnydotdev.github.io/twinny-docs/'
+export const URL_RJMACARTHY = 'https://x.com/twinnydotdev'
 
