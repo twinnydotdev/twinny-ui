435ad748002a364eefc83a1f9b421cc9fb4a8c95
rjmacarthy <richardmacarthy@hotmail.com>
2024-11-17 19:20:55 +0000

    update symmetry page

--- src/routes/symmetry/+page.svelte
+++ src/routes/symmetry/+page.svelte
@@ -2,7 +2,12 @@
   import { t } from '$lib/translations'
   import Register from '$lib/components/register.svelte'
   import { onMount, onDestroy } from 'svelte'
-  import { URL_SYMMETRY_CLI, URL_SYMMETRY_CORE, URL_SYMMETRY_DOCS, URL_SYMMETRY_SERVER } from '$lib/const'
+  import {
+    URL_SYMMETRY_CLI,
+    URL_SYMMETRY_CORE,
+    URL_SYMMETRY_DOCS,
+    URL_SYMMETRY_SERVER
+  } from '$lib/const'
   import type { Peer } from 'symmetry-core'
 
   let ws: WebSocket
@@ -10,17 +15,16 @@
   let activeModels = 0
   let peers: any[] = []
 
-  const sortPeers = (peers: any): any => {
+  const sortPeers = (peers: any[]): any[] => {
     return [...peers].sort((a, b) => {
       if (a.online !== b.online) {
-        return b.online - a.online;
+        return b.online - a.online
       }
-
-      const pointsA = a.points ?? 0;
-      const pointsB = b.points ?? 0;
-      return pointsB - pointsA;
-    });
-  };
+      const pointsA = a.points ?? 0
+      const pointsB = b.points ?? 0
+      return pointsB - pointsA
+    })
+  }
 
   onMount(() => {
     ws = new WebSocket('wss://twinny.dev/ws')
@@ -30,7 +34,6 @@
       activePeers = data.activePeers
       activeModels = data.activeModels
       peers = sortPeers(data.allPeers)
-      console.log(peers)
     }
 
     ws.onerror = (error) => {
@@ -46,51 +49,51 @@
 </script>
 
 <template lang="pug">
-div(class="min-h-screen flex flex-col items-center p-4")
-  div(class="w-full max-w-4xl")
-    h2(class="text-3xl font-bold") {$t('common.symmetry')}
-    p(class="py-6") {$t('common.symmetry_description')}
-    p(class="pb-4")
-      span {$t('common.symmetry_learn_more')}
-      a(href='{URL_SYMMETRY_CORE}' class="text-rose-500 ml-1" target="_blank") {$t('common.check_out_the_code_client')}
-    p(class="pb-6")
-      span {$t('common.symmetry_server_learn_more')}
-      a(href='{URL_SYMMETRY_SERVER}' class="text-rose-500 ml-1" target="_blank") {$t('common.check_out_the_code_server')}
-    h3(class="text-xl font-semibold mb-4") {$t('common.become_a_provider')}
-    p(class="pb-2 font-medium mt-2")
-      span {$t('common.become_a_provider_description')}
-      a(href='{URL_SYMMETRY_CORE}' class="text-rose-500 ml-1" target="_blank") {$t('common.symmetry_installation')}
-    p(class="pb-2 font-medium mt-2")
-      span {$t('common.symmetry_cli')}
-      a(href='{URL_SYMMETRY_CLI}' class="text-rose-500 ml-1" target="_blank") {$t('common.symmetry_installation_cli')}
-    div(class="mb-6 mt-2")
-      h3(class="text-xl font-semibold mb-4") {$t('common.active_connections')}
+div
+  div
+    div(class="mb-8")
+      h3(class="text-2xl font-semibold mb-4") {$t('common.active_connections')}
       div(class="grid grid-cols-1 sm:grid-cols-2 gap-4")
-        div(class="bg-stone-800 p-4 rounded-md")
-          p(class="text-sm font-medium") {$t('common.active_peers')}
-          p(class="text-2xl font-bold") {activePeers}
-        div(class="bg-stone-800 p-4 rounded-md")
-          p(class="text-sm font-medium") {$t('common.active_models')}
-          p(class="text-2xl font-bold") {activeModels}
+        div(class="bg-stone-800 p-6 rounded-lg")
+          p(class="text-sm font-medium mb-2") {$t('common.active_peers')}
+          p(class="text-3xl font-bold") {activePeers}
+        div(class="bg-stone-800 p-6 rounded-lg")
+          p(class="text-sm font-medium mb-2") {$t('common.active_models')}
+          p(class="text-3xl font-bold") {activeModels}
+
     div(class="mb-6")
-      h3(class="text-xl font-semibold mb-4") {$t('common.providers')}
-      div(class="overflow-x-auto")
-        table(class="w-full bg-stone-800 rounded-lg overflow-hidden")
-          thead(class="bg-stone-700")
-            tr
-              th(class="px-4 py-2 text-left text-sm font-semibold") {$t('common.model')}
-              th(class="px-4 py-2 text-left text-sm font-semibold sm:table-cell") {$t('common.name')}
-              th(class="px-4 py-2 text-left text-sm font-semibold sm:table-cell") {$t('common.online')}
-              th(class="px-4 py-2 text-left text-sm font-semibold hidden xl:table-cell") {$t('common.data_collected')}
-              th(class="px-4 py-2 text-left text-sm font-semibold hidden lg:table-cell") {$t('common.provider')}
-              th(class="px-4 py-2 text-left text-sm font-semibold hidden lg:table-cell") {$t('common.points')}
-          tbody
-            +each('peers as peer')
-              tr(class="border-t border-stone-700")
-                td(class="px-4 py-2") {peer.model_name}
-                td(class="px-4 py-2 sm:table-cell") {peer.name || 'N/A'}
-                td(class="px-4 py-2 sm:table-cell") {peer.online ? 'online' : '-'}
-                td(class="px-4 py-2 hidden sm:table-cell") {peer.data_collection_enabled ? $t('common.yes') : $t('common.no')}
-                td(class="px-4 py-2 hidden sm:table-cell") {peer.provider || 'unknown'}
-                td(class="px-4 py-2 hidden sm:table-cell") {peer.points || 0}
+      div(class="flex justify-between items-center mb-4")
+        h3(class="text-2xl font-semibold") {$t('common.providers')}
+
+    div(class="overflow-x-auto")
+      table(class="w-full min-w-full bg-stone-800 rounded-lg overflow-hidden  text-sm")
+        thead(class="bg-stone-700")
+          tr
+            th(class="px-2 py-3 text-left text-sm font-semibold") {$t('common.model')}
+            th(class="px-2 py-3 text-left text-sm font-semibold") {$t('common.name')}
+            th(class="px-2 py-3 text-left text-sm font-semibold") {$t('common.online')}
+            th(class="px-2 py-3 text-left text-sm font-semibold hidden xl:table-cell") {$t('common.data_collected')}
+            th(class="px-2 py-3 text-left text-sm font-semibold hidden lg:table-cell") {$t('common.provider')}
+            th(class="px-2 py-3 text-left text-sm font-semibold") {$t('common.points')}
+        tbody
+          +each('peers as peer')
+            tr(class="border-t border-stone-700 hover:bg-stone-700/50 transition-colors")
+              td(class="px-2 py-3") {peer.model_name}
+              td(class="px-2 py-3") {peer.name || 'N/A'}
+              td(class="px-2 py-3")
+                span(class="inline-flex items-center px-2 py-1 rounded-full font-medium")
+                  span(class="{peer.online ? 'text-green-600' : 'text-red-600'}")  {peer.online ? '✔' : '✕'}
+              td(class="px-2 py-3 hidden xl:table-cell") {peer.data_collection_enabled ? $t('common.yes') : $t('common.no')}
+              td(class="px-2 py-3 hidden lg:table-cell") {peer.provider || 'unknown'}
+              td(class="px-2 py-3")
+                span(class="font-medium") {peer.points || 0}
+
+    h3(class="text-2xl font-semibold mb-4 mt-6") {$t('common.become_a_provider')}
+
+    div(class="bg-stone-800 rounded-lg p-6 mb-8")
+      p(class="mb-4") {$t('common.become_a_provider_description')}
+      div(class="space-y-2")
+        a(href='{URL_SYMMETRY_CORE}' class="text-rose-500 hover:text-rose-400 block") {$t('common.symmetry_installation')}
+        a(href='{URL_SYMMETRY_CLI}' class="text-rose-500 hover:text-rose-400 block") {$t('common.symmetry_installation_cli')}
+
 </template>
