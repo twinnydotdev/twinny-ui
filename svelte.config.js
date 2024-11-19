66036450ecbac099227bc7781f6daa91fc06f8f9
rjmacarthy <richardmacarthy@hotmail.com>
2024-11-19 12:28:24 +0000

    update shit

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,12 @@
-import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-node'
 
 import { sveltekit } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: [
     sveltekit(),
-    preprocess({
-      postcss: true
-    })
   ],
-  compilerOptions: {
-    dev: process.env.DEVELOPMENT
-  },
   kit: {
     adapter: adapter()
   }
