870d9fbc01a5faae2028ab51768f43c01a189533
rjmacarthy <richardmacarthy@hotmail.com>
2024-12-04 11:22:38 +0000

    update docs url

--- src/lib/const/index.ts
+++ src/lib/const/index.ts
@@ -1,10 +1,10 @@
 export const URL_VSCODE_MARKETPLACE =
   'https://marketplace.visualstudio.com/items?itemName=rjmacarthy.Twinny'
-export const URL_DOCS = 'https://twinnydotdev.github.io/twinny-docs/'
+export const URL_DOCS = 'https://docs.twinny.dev'
 export const URL_GITHUB = 'https://github.com/twinnydotdev'
 export const URL_TWINNYDOTDEV = 'https://x.com/twinnydotdev'
 export const URL_SYMMETRY_CORE = 'https://github.com/twinnydotdev/symmetry-core'
 export const URL_SYMMETRY_SERVER = 'https://github.com/twinnydotdev/symmetry-server'
 export const URL_SYMMETRY_CLI = 'https://github.com/twinnydotdev/symmetry-cli'
-export const URL_SYMMETRY_DOCS = 'https://twinnydotdev.github.io/twinny-docs/general/symmetry'
+export const URL_SYMMETRY_DOCS = 'https://docs.twinny.dev/general/symmetry'
 export const API_URL = 'https://twinny.dev'
