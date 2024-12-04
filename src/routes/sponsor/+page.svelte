702fac323f44ee62e83334c908d1fe837696fe58
rjmacarthy <richardmacarthy@hotmail.com>
2024-12-04 21:58:48 +0000

    remove

--- src/routes/sponsor/+page.svelte
+++ src/routes/sponsor/+page.svelte
@@ -67,21 +67,3 @@
     <div class="text-sm text-green-400">Address copied to clipboard!</div>
   {/if}
 </div>
-
-<style lang="postcss">
-  /* Optional: Add any component-specific styles here */
-  .fade-enter {
-    opacity: 0;
-  }
-  .fade-enter-active {
-    opacity: 1;
-    transition: opacity 150ms ease-out;
-  }
-  .fade-exit {
-    opacity: 1;
-  }
-  .fade-exit-active {
-    opacity: 0;
-    transition: opacity 150ms ease-in;
-  }
-</style>
