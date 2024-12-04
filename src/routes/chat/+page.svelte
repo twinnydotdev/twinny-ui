5d82a1437e4d266d651637b10bc4b437f8b7f637
rjmacarthy <richardmacarthy@hotmail.com>
2024-12-04 11:26:45 +0000

    fix api urls

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -40,7 +40,7 @@
       messages = [...messages, { role: 'user', content: message }]
       message = ''
       chatContainer.scrollTo({ top: chatContainer.scrollHeight })
-      const response = await fetch('http://localhost:4005/v1/chat/completions', {
+      const response = await fetch('https://twinny.dev/v1/chat/completions', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
