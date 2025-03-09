f3890e0139a2680395c98510f20f89a56e1a6c65
rjmacarthy <richardmacarthy@hotmail.com>
2025-03-09 11:25:32 +0000

    oai compatiable

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -41,10 +41,8 @@
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          sessionRequest: { modelName: model },
-          data: {
-            messages
-          }
+          model,
+          messages
         })
       })
 
